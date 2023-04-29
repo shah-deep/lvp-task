c489f5256a7f92dac04cdf6ea587919c02c757bb
shah-deep2 <deepshah11111@gmail.com>
2023-04-29 01:41:00 -0700

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,2 +1,2 @@
-export const SERVER_URL = 'http://ec2-34-230-30-53.compute-1.amazonaws.com:5000';
+export const SERVER_URL = 'http://ec2-54-204-128-60.compute-1.amazonaws.com:5000';
 // export const SERVER_URL = 'http://localhost:5000';
